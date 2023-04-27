a0789caef3c9af8c33267b6ae5d79aea835ab5f3
rail5 <andrew@rail5.org>
2023-04-27 16:26:26 -0400

    Updated setup.php

--- setup.php
+++ setup.php
@@ -14,20 +14,18 @@ if (ini_get('file_uploads') != 1) {
 
 // Check for SQLite and PDO
 if (!extension_loaded("pdo_sqlite")) {
-	echo '<div align="center"><h1><font color="FF0000">Warning: You do not have the PHP SQLite extension installed.</font></h1><h3>Please install the PHP sqlite3 extension before moving forward</h3></div><br><br>';
+	echo '<hr><br><div align="center"><h1><font color="FF0000">Warning: You do not have the PHP SQLite extension installed.</font></h1><h3>Please install the PHP sqlite3 extension before moving forward</h3></div><br><br>';
 }
 
 if (!extension_loaded("zip")) {
-	echo '<div align="center"><h1><font color="FF0000">Warning: You do not have the PHP ZIP extension installed.</font></h1><h3>Please install the PHP-zip extension before moving forward</h3></div><br><br>';
+	echo '<hr><br><div align="center"><h1><font color="FF0000">Warning: You do not have the PHP ZIP extension installed.</font></h1><h3>Please install the PHP-zip extension before moving forward</h3></div><br><br>';
 }
 
-echo '<div align="center"><b><u>php.ini</u></b> specifies your server\'s <i>maximum upload filesize</i> as:<b> '.ini_get('upload_max_filesize').'</b></div><br>';
+echo '<hr><br><div align="center"><h3><b><u>php.ini</u></b> specifies your server\'s <i>maximum upload filesize</i> as: <u><b>'.min(ini_get('post_max_size'), ini_get('upload_max_filesize')).'</b></u></h3><br><br>To change this, edit the value of <b>upload_max_filesize</b> <i>(currently '.ini_get('upload_max_filesize').')</i> in your <u>php.ini</u><br>The value of <b>post_max_size</b> <i>(currently '.ini_get('post_max_size').')</i> must always be <u>larger</u> than <b>upload_max_filesize</b></div><br><hr><br>';
 
-echo '<div align="center"><b><u>php.ini</u></b> specifies that your server can permit uploads of up to <b> '.ini_get('max_file_uploads').' files</b>  in one request</div><br>';
+echo '<div align="center"><h3><b><u>php.ini</u></b> specifies that your server can permit uploads of up to <u><b> '.ini_get('max_file_uploads').' files</b></u>  in one request</h3><br><br>To change this, edit the value of <b>max_file_uploads</b> in your <u>php.ini</u></div><br><hr><br>';
 ?>
 	<div align="center">
-		<h1>IMPORTANT:</h1>
-		<h2>Delete this file <i>immediately</i> after completing set-up</h2><br />
 		<form action="setup.php" method="post">
 		<input type="hidden" name="formsubmitted" id="formsubmitted" value="true">
 Create admin <i>(uploader)</i> account
@@ -36,7 +34,7 @@ Create admin <i>(uploader)</i> account
 		<br>
 <input type="password" name="password" id="password" placeholder="Admin password">
 		<br>
-<input type="checkbox" name="makeuser" id="makeuser" onchange="seconduser(this)"> Create a second user who can also upload stuff?
+<input type="checkbox" name="makeuser" id="makeuser" onchange="seconduser(this)"> Create a second user who can also upload files?
 		<div id="seconduser">
 			<input type="text" name="user2" id="user2" placeholder="Second username">
 		<br>
